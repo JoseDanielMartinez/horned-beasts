2512f92d160bff74a8b7e35e67d95d10953a9fcd
Jose Daniel Martinez <jmartinez42528@gmail.com>
2023-01-11 04:01:11 +0000

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Main extends React.Component{
+  render(){
+   return (
+    <>
+    <h3>Hello Beasts</h3>
+    <article>
+      <p>Jose</p>
+    </article>
+    </>
+   )
+  }
+}
+
+
+export default Main;
